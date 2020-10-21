21ba4daf6224271fac342c5c8dd9e8e51f02bcba
Vicky <vlcabrera92@hotmail.com>
2020-10-20 21:21:43 -0300

    Updates

---
+++ precache-manifest.ad5ff1018f5b7d102b27da7ee585db68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a9467d1cec0dde09867218836e59939",
+    "url": "/administrador-citas/index.html"
+  },
+  {
+    "revision": "c44faae2f6dea66f702e",
+    "url": "/administrador-citas/static/css/main.c4277b39.chunk.css"
+  },
+  {
+    "revision": "1f6987cc6fa5e702eb22",
+    "url": "/administrador-citas/static/js/2.f32d493b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/administrador-citas/static/js/2.f32d493b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c44faae2f6dea66f702e",
+    "url": "/administrador-citas/static/js/main.241ecf4e.chunk.js"
+  },
+  {
+    "revision": "6896db0dd4f231e29124",
+    "url": "/administrador-citas/static/js/runtime-main.1c7e4d0e.js"
+  }
+]);
